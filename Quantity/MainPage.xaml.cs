9757f236f809de61ffdb7671cceee5f293ff02bd
Vitor Campos <vitormcampos99@gmail.com>
2022-01-09 12:21:55 -0300

    Add project files.

---
+++ Quantity/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Quantity
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Clicked(object sender, EventArgs e)
+        {
+            lblCounter.Text = $"{ int.Parse(lblCounter.Text) + 1 }";
+        }
+
+        private void btnMinus_Clicked(object sender, EventArgs e)
+        {
+            lblCounter.Text = $"{ int.Parse(lblCounter.Text) - 1 }";
+        }
+    }
+}
